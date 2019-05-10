0b6ded80f4ebf3da537f6eccc2f42897c39d6350
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-10 18:57:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -19,7 +19,7 @@ end
 
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-counter = 1
+counter = 7
 while coutner <= number_of_times
   puts phrase
   counter += 1
