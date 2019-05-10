6854a24e7cbd7eeed1fceeab354dc89618a2684e
Catherine Cherubin <cat.cherubin@gmail.com>
2019-05-10 18:53:29 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,7 @@
 def loop_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
-  loop do 
+  loop do
     puts phrase
     counter += 1
     break if counter == number_of_times
